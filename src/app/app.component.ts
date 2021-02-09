0aad0d59e22c258bef477f3753134fbbc3eeebe4
sarastackhouse <sarapalacios7@gmail.com>
2021-02-09 20:24:16 +0100

    first component counter-app created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,5 @@ import { Component } from '@angular/core';
   // `
 })
 export class AppComponent {
-  title : string = 'Contador App';
-  numero: number = 10;
-  base  : number = 5;
 
-  acumular = (valor: number) => {
-    this.numero += valor;
-  };
 };
