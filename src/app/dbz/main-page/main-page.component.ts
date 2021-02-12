83575f1546592ba576d1c42dedd956be9fb3d7da
sarastackhouse <sarapalacios7@gmail.com>
2021-02-12 19:00:22 +0100

    ngmodel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: String,
+  poder: Number
+};
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Trucks',
+    poder:14000
+  };
+
   agregrar() {
-    console.log('Esta es una prueba');
-  }
+    console.log(this.nuevo);
+  };
 };
